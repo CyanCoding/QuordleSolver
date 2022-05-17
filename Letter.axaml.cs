9743ea060b7e72d27c6913122a71d13dfab0d528
Cam Mac Leod <camden.macleod@pm.me>
2022-05-17 15:40:48 -0700

    Update

--- Letter.axaml.cs
+++ Letter.axaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Security.Cryptography;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
@@ -10,12 +12,28 @@ public partial class Letter : UserControl {
     private readonly IBrush? _yellowBrush  = new BrushConverter().ConvertFrom("#ffcc00") as Brush;
     private readonly IBrush? _greenBrush  = new BrushConverter().ConvertFrom("#00cc88") as Brush;
     private readonly IBrush? _greyBrush  = new BrushConverter().ConvertFrom("#9ca3af") as Brush;
+    
+    private readonly IBrush? _disabledBrush  = new BrushConverter().ConvertFrom("#155e75") as Brush;
+    private readonly IBrush? _enabledBrush  = new BrushConverter().ConvertFrom("#9ca3af") as Brush;
 
     private readonly Label _text;
+    private readonly Border _coverBorder;
+    private readonly Border[] _borders;
+    
     public Letter() {
         InitializeComponent();
 
         _text = this.FindControl<Label>("Text");
+        _coverBorder = this.FindControl<Border>("CoverBorder");
+
+        _borders = new[] {
+            this.FindControl<Border>("Border1"),
+            this.FindControl<Border>("Border2"),
+            this.FindControl<Border>("Border3"),
+            this.FindControl<Border>("Border4")
+        };
+        
+
     }
     
     /// <summary>
@@ -30,22 +48,98 @@ public partial class Letter : UserControl {
         AvaloniaXamlLoader.Load(this);
     }
 
+    /// <summary>
+    /// Returns an int[4] array of the border status on the letter.
+    /// NULL = disabled letter
+    /// 0 = grey border
+    /// 1 = yellow border
+    /// 2 = green border
+    /// </summary>
+    internal int[]? GetArrangement() {
+        // The letter isn't anywhere
+        if (Equals(_text.Background, _disabledBrush)) {
+            return null;
+        }
+
+        var sides = new int[4];
+
+        for (int i = 0; i < 4; i++) {
+            sides[i] = 0;
+            var label = (Label)_borders[i].Child;
+            if (label.Content.ToString() == "?") {
+                throw new Exception("Cannot have question marks!");
+            }
+            
+            if (Equals(_borders[i].BorderBrush, _yellowBrush)) {
+                sides[i] = 1;
+            }
+            else if (Equals(_borders[i].BorderBrush, _greenBrush)) {
+                sides[i] = 2;
+            }
+        }
+
+        return sides;
+    }
+
+    /// <summary>
+    /// Sets each yellow border to '?' once it has stored the current value
+    /// </summary>
+    internal void ResetYellow() {
+        for (int i = 0; i < 4; i++) {
+            if (Equals(_borders[i].BorderBrush, _yellowBrush)) {
+                var label = (Label)_borders[i].Child;
+                label.Content = "?";
+            }
+        }
+    }
+
     private void Border_Pressed(object? sender, PointerPressedEventArgs e) {
         var border = (Border)sender!;
         if (Equals(border.BorderBrush, _greyBrush)) {
             border.BorderBrush = _yellowBrush;
+            var label = (Label)border.Child;
+            label.Content = 1;
             return;
         }
         if (Equals(border.BorderBrush, _yellowBrush)) {
+            var label = (Label)border.Child;
+            if (label.Content.ToString() == "  " || label.Content.ToString() == "?") {
+                label.Content = 1;
+                return;
+            }
+            if (int.Parse(label.Content.ToString() ?? "0") < 5) {
+                label.Content = int.Parse(label.Content.ToString() ?? "0") + 1;
+                return;
+            }
             border.BorderBrush = _greenBrush;
+            label.Content = "1";
             return;
         }
         if (Equals(border.BorderBrush, _greenBrush)) {
+            var label = (Label)border.Child;
+            if (int.Parse(label.Content.ToString() ?? "0") < 5) {
+                label.Content = int.Parse(label.Content.ToString() ?? "0") + 1;
+                return;
+            }
+
+            label.Content = "  ";
             border.BorderBrush = _greyBrush;
             return;
         }
         
         // If we got here it means none triggered (e.g. it's in the normal border state) so we set to yellow
         border.BorderBrush = _yellowBrush;
+        var label1 = (Label)border.Child;
+        label1.Content = 1;
+    }
+
+    private void Text_OnPointerPressed(object? sender, PointerPressedEventArgs e) {
+        if (Equals(_text.Background, _disabledBrush)) {
+            _text.Background = _enabledBrush;
+            _coverBorder.IsVisible = false;
+            return;
+        }
+        _text.Background = _disabledBrush;
+        _coverBorder.IsVisible = true;
     }
 }
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,9 +1,138 @@
+using System;
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace QuordleSolver {
     public partial class MainWindow : Window {
+        private readonly Label _mainLabel;
+        private int round = 0;
+
+        private readonly Letter[] _letters;
         public MainWindow() {
             InitializeComponent();
+
+            _mainLabel = this.FindControl<Label>("MainLabel");
+
+            _letters = new[] {
+                this.FindControl<Letter>("A"),
+                this.FindControl<Letter>("B"),
+                this.FindControl<Letter>("C"),
+                this.FindControl<Letter>("D"),
+                this.FindControl<Letter>("E"),
+                this.FindControl<Letter>("F"),
+                this.FindControl<Letter>("G"),
+                this.FindControl<Letter>("H"),
+                this.FindControl<Letter>("I"),
+                this.FindControl<Letter>("J"),
+                this.FindControl<Letter>("K"),
+                this.FindControl<Letter>("L"),
+                this.FindControl<Letter>("M"),
+                this.FindControl<Letter>("N"),
+                this.FindControl<Letter>("O"),
+                this.FindControl<Letter>("P"),
+                this.FindControl<Letter>("Q"),
+                this.FindControl<Letter>("R"),
+                this.FindControl<Letter>("S"),
+                this.FindControl<Letter>("T"),
+                this.FindControl<Letter>("U"),
+                this.FindControl<Letter>("V"),
+                this.FindControl<Letter>("W"),
+                this.FindControl<Letter>("X"),
+                this.FindControl<Letter>("Y"),
+                this.FindControl<Letter>("Z"),
+            };
+        }
+
+        private List<string> illegalLetters;
+        private List<string> requiredLetters;
+        private void Button_OnClick(object? sender, RoutedEventArgs e) {
+            string suggestion = "";
+            var alphabet = new[] {
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "i",
+                "j",
+                "k",
+                "l",
+                "m",
+                "n",
+                "o",
+                "p",
+                "q",
+                "r",
+                "s",
+                "t",
+                "u",
+                "v",
+                "w",
+                "x",
+                "y",
+                "z"
+            };
+            
+            // Get the configuration of each letter on keyboard
+            int[]?[] letterArrangements;
+            try {
+                letterArrangements = new[] {
+                    _letters[0].GetArrangement(),
+                    _letters[1].GetArrangement(),
+                    _letters[2].GetArrangement(),
+                    _letters[3].GetArrangement(),
+                    _letters[4].GetArrangement(),
+                    _letters[5].GetArrangement(),
+                    _letters[6].GetArrangement(),
+                    _letters[7].GetArrangement(),
+                    _letters[8].GetArrangement(),
+                    _letters[9].GetArrangement(),
+                    _letters[10].GetArrangement(),
+                    _letters[11].GetArrangement(),
+                    _letters[12].GetArrangement(),
+                    _letters[13].GetArrangement(),
+                    _letters[14].GetArrangement(),
+                    _letters[15].GetArrangement(),
+                    _letters[16].GetArrangement(),
+                    _letters[17].GetArrangement(),
+                    _letters[18].GetArrangement(),
+                    _letters[19].GetArrangement(),
+                    _letters[20].GetArrangement(),
+                    _letters[21].GetArrangement(),
+                    _letters[22].GetArrangement(),
+                    _letters[23].GetArrangement(),
+                    _letters[24].GetArrangement(),
+                    _letters[25].GetArrangement()
+                };
+            }
+            catch (Exception) {
+                // Occurs if there are question marks on the board
+                _mainLabel.Content = "YOU CANNOT HAVE QUESTION MARKS!";
+                return;
+            }
+            
+
+            // Add letters to illegal list
+            for (int i = 0; i < 26; i++) {
+                if (letterArrangements[i] == null) {
+                    illegalLetters.Add(alphabet[i]);
+                }
+            }
+            
+            // Add letters to required list (all sides green)
+            
+            // Reset the yellow values after recording letter placement
+            foreach (var letter in _letters) {
+                letter.ResetYellow();
+            }
+
+            // Update suggestion
+            round++;
+            _mainLabel.Content = "Round: " + round + " - Try: " + suggestion;
         }
     }
 }
